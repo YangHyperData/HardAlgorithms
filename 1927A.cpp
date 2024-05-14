1a444a8e09dfcb7d71d906461f9e026d89fe62fd
YangStone <131573711+YangHyperData@users.noreply.github.com>
2024-05-14 19:20:45 +0700

    1927A - Make it White

--- 1927A.cpp
+++ 1927A.cpp
@@ -3,9 +3,11 @@
 #include <cstdio>
 #include <vector>
 #include <bits/stdc++.h>
+#include <conio.h>
 
 using namespace std;
 
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define nl << '\n'
 #define vti vector<int>
 #define vtll vector<long long>
@@ -24,8 +26,12 @@ using namespace std;
 #define ed end()
 #define rbg rbegin()
 #define red rend()
-#define sz size()
-#define RFOR(i, a, b) for (int(i) = (a)-1; (i) >= (b); --(i))
+#define dbg(x...)        \
+	cerr << #x << " : "; \
+	_print(x)
+#define uceil(a, b) ((a + b - 1) / (b))
+#define dbg(...)
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define sp << " "
 #define fr(i, l, r) for (int i = l; i <= r; i++)
 #define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
@@ -41,14 +47,14 @@ using namespace std;
 #define rsrt(x) sort(x.rbg, x.red)
 #define all(r) r.begin(), r.end()
 #define vi vector<int>
-
+const int MAXN = 4e5 + 5;
 #define rall(r) r.rbegin(), r.rend()
 #define fast_out()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL)
 const int mod = 1e9 + 7;
 const int N = 100010;
-const ll INF = 2e18 + 99;
+const ll INF = 1e9 + 7;
 
 // void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
 int bin_pow(int a, int b, int mod) // luy thua nhi phan
@@ -62,34 +68,22 @@ int bin_pow(int a, int b, int mod) // luy thua nhi phan
 	}
 	return (a * (tmp * tmp) % mod) % mod;
 }
-// vector<int> factorize(int n)
-// {
-//     vector <int> res;
-//     for (int i=2; i*i<=n; i++)
-//         {
-//         while (n%i==0)
-//             {
-//             res.push_back(i);
-//             n/=i;
-//         }
-//     }
-//     if (n!=1) res.push_back(n);
-//     return res;
-// }
-// vector<int> factorize(int n)
-// {
-//     vector <int> res;
-//     for (int i=2; i*i<=n; i++)
-//         {
-//         while (n%i==0)
-//             {
-//             res.push_back(i);
-//             n/=i;
-//         }
-//     }
-//     if (n!=1) res.push_back(n);
-//     return res;
-// }
+
+vector<int> factorize(int n)
+{
+	vector<int> res;
+	for (int i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			res.push_back(i);
+			n /= i;
+		}
+	}
+	if (n != 1)
+		res.push_back(n);
+	return res;
+}
 //* Delete
 /*var a = document.querySelectorAll(".MJX_Assistive_MathML")
 a.forEach(s=> s.remove())
@@ -115,7 +109,30 @@ bool isPerfectSquare(int n)
 	int rt = sqrt(n);
 	return (rt * rt == n);
 }
+bool isPrime(int n)
+{
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
 
+	return true;
+}
+ll func(ll n)
+{
+	ll sum = 0;
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+bool cmp(ll l, ll r)
+{
+	return l > r;
+}
 void solve()
 {
 	ll n;
@@ -146,18 +163,15 @@ void solve()
 
 	cout << last - first + 1 << endl;
 }
-
 int main()
 {
-
 	fast_out();
 	int t = 1;
 	cin >> t;
 	wh(t--)
 	{
 		solve();
-
-		//cout << endl;
+		// cout << endl;
 	}
 
 	return 0;
