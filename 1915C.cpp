66832cf786046c5e02b9d1cabaf47aff6107c38b
YangStone <131573711+YangHyperData@users.noreply.github.com>
2024-05-31 17:30:46 +0700

    1915C - Can I Square?

--- 1915C.cpp
+++ 1915C.cpp
@@ -3,8 +3,11 @@
 #include <cstdio>
 #include <vector>
 #include <bits/stdc++.h>
+#include <conio.h>
+
 using namespace std;
 
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define nl << '\n'
 #define vti vector<int>
 #define vtll vector<long long>
@@ -23,7 +26,12 @@ using namespace std;
 #define ed end()
 #define rbg rbegin()
 #define red rend()
-#define sz size()
+#define dbg(x...)        \
+	cerr << #x << " : "; \
+	_print(x)
+#define uceil(a, b) ((a + b - 1) / (b))
+#define dbg(...)
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define sp << " "
 #define fr(i, l, r) for (int i = l; i <= r; i++)
 #define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
@@ -39,15 +47,17 @@ using namespace std;
 #define rsrt(x) sort(x.rbg, x.red)
 #define all(r) r.begin(), r.end()
 #define vi vector<int>
+const int MAXN = 4e5 + 5;
 #define rall(r) r.rbegin(), r.rend()
 #define fast_out()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL)
 const int mod = 1e9 + 7;
 const int N = 100010;
-const ll INF = 2e18 + 99;
+const ll INF = 1e9 + 7;
 
-int bin_pow(int a, int b, int mod)
+// void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
+int bin_pow(int a, int b, int mod) // luy thua nhi phan
 {
 	if (b == 0)
 		return 1;
@@ -58,6 +68,22 @@ int bin_pow(int a, int b, int mod)
 	}
 	return (a * (tmp * tmp) % mod) % mod;
 }
+
+vector<int> factorize(int n)
+{
+	vector<int> res;
+	for (int i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			res.push_back(i);
+			n /= i;
+		}
+	}
+	if (n != 1)
+		res.push_back(n);
+	return res;
+}
 //* Delete
 /*var a = document.querySelectorAll(".MJX_Assistive_MathML")
 a.forEach(s=> s.remove())
@@ -66,8 +92,6 @@ bool compare(string &a, string &b)
 {
 	return a + b > b + a;
 }
-vector<ll> a[1001];
-bool visited[1001];
 bool isPerfectSquare(ll n)
 {
 	ll temp = sqrt(n);
@@ -76,11 +100,57 @@ bool isPerfectSquare(ll n)
 		return true;
 	return false;
 }
+bool check(char r)
+{
+	return (r == 'a' || r == 'e');
+}
+bool isPerfectSquare(int n)
+{
+	int rt = sqrt(n);
+	return (rt * rt == n);
+}
+bool isPrime(int n)
+{
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+ll func(ll n)
+{
+	ll sum = 0;
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+bool cmp(ll l, ll r)
+{
+	return l > r;
+}
+ll binpow(ll a, ll b)
+{
+	ll ans = 1ll;
+	while (b > 0)
+	{
+		if (b & 1)
+			ans *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return ans;
+}
+
 void solve()
 {
 	ll n;
 	cin >> n;
-
+ 
 	ll sum = 0;
 	for (int i = 0; i < n; i++)
 	{
@@ -88,7 +158,7 @@ void solve()
 		cin >> temp;
 		sum += temp;
 	}
-
+ 
 	if (isPerfectSquare(sum))
 		cout << "YES" << endl;
 	else
@@ -96,14 +166,12 @@ void solve()
 }
 int main()
 {
-
 	fast_out();
 	int t = 1;
 	cin >> t;
 	wh(t--)
 	{
 		solve();
-
 		// cout << endl;
 	}
 
