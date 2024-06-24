d0e36b5e8d12577ec31de8da41efe19d2565f223
YangStone <131573711+YangHyperData@users.noreply.github.com>
2024-06-24 12:02:14 +0700

    1933A - Turtle Puzzle: Rearrange and Negate

--- 1933A.cpp
+++ 1933A.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-#define RFOR(i, a, b) for (int(i) = (a)-1; (i) >= (b); --(i))
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define nl << '\n'
 #define vti vector<int>
 #define vtll vector<long long>
@@ -16,7 +16,7 @@ using namespace std;
 #define ll long long
 #define lg long
 #define db double
-#define MOD 1000000007
+#define MOD 998244353
 #define dbg(x)         \
 	cerr << #x << " "; \
 	_print(x);         \
@@ -29,9 +29,9 @@ using namespace std;
 #define dbg(x...)        \
 	cerr << #x << " : "; \
 	_print(x)
-
+#define uceil(a, b) ((a + b - 1) / (b))
 #define dbg(...)
-#define RFOR(i, a, b) for (int(i) = (a)-1; (i) >= (b); --(i))
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define sp << " "
 #define fr(i, l, r) for (int i = l; i <= r; i++)
 #define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
@@ -47,14 +47,15 @@ using namespace std;
 #define rsrt(x) sort(x.rbg, x.red)
 #define all(r) r.begin(), r.end()
 #define vi vector<int>
-
+const int MAXN = 4e5 + 5;
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
 #define rall(r) r.rbegin(), r.rend()
 #define fast_out()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL)
 const int mod = 1e9 + 7;
 const int N = 100010;
-const ll INF = 2e18 + 99;
+const ll INF = 1e9 + 7;
 
 // void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
 int bin_pow(int a, int b, int mod) // luy thua nhi phan
@@ -129,13 +130,30 @@ ll func(ll n)
 	}
 	return sum;
 }
+bool cmp(ll l, ll r)
+{
+	return l > r;
+}
+ll binpow(ll a, ll b)
+{
+	ll ans = 1ll;
+	while (b > 0)
+	{
+		if (b & 1)
+			ans *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return ans;
+}
 void solve()
 {
 	ll n;
 	cin >> n;
 	ll a[n];
 	ll sum = 0;
-	for(ll i = 0 ; i < n ;i++){
+	for (ll i = 0; i < n; i++)	
+	{
 		cin >> a[i];
 		sum += abs(a[i]);
 	}
@@ -143,14 +161,12 @@ void solve()
 }
 int main()
 {
-
 	fast_out();
 	int t = 1;
 	cin >> t;
 	wh(t--)
 	{
 		solve();
-
 		// cout << endl;
 	}
 
