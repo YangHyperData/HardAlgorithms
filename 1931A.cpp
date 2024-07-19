429492c1c662dd6a533726e220c01c21844ea3e7
YangStone <131573711+YangHyperData@users.noreply.github.com>
2024-07-19 08:37:55 +0700

    1931A - Recovering a Small String

--- 1931A.cpp
+++ 1931A.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <bits/stdc++.h>
 #include <conio.h>
-
+#include <algorithm>
 using namespace std;
 
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define nl << '\n'
 #define vti vector<int>
 #define vtll vector<long long>
@@ -15,7 +16,7 @@ using namespace std;
 #define ll long long
 #define lg long
 #define db double
-#define MOD 1000000007
+#define MOD 998244353
 #define dbg(x)         \
 	cerr << #x << " "; \
 	_print(x);         \
@@ -25,8 +26,12 @@ using namespace std;
 #define ed end()
 #define rbg rbegin()
 #define red rend()
-
-#define RFOR(i, a, b) for (int(i) = (a)-1; (i) >= (b); --(i))
+#define dbg(x...)        \
+	cerr << #x << " : "; \
+	_print(x)
+#define uceil(a, b) ((a + b - 1) / (b))
+#define dbg(...)
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
 #define sp << " "
 #define fr(i, l, r) for (int i = l; i <= r; i++)
 #define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
@@ -42,14 +47,15 @@ using namespace std;
 #define rsrt(x) sort(x.rbg, x.red)
 #define all(r) r.begin(), r.end()
 #define vi vector<int>
-
+const int MAXN = 4e5 + 5;
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
 #define rall(r) r.rbegin(), r.rend()
 #define fast_out()                    \
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL)
 const int mod = 1e9 + 7;
 const int N = 100010;
-const ll INF = 2e18 + 99;
+const ll INF = 1e9 + 7;
 
 // void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
 int bin_pow(int a, int b, int mod) // luy thua nhi phan
@@ -63,34 +69,22 @@ int bin_pow(int a, int b, int mod) // luy thua nhi phan
 	}
 	return (a * (tmp * tmp) % mod) % mod;
 }
-// vector<int> factorize(int n)
-// {
-//     vector <int> res;
-//     for (int i=2; i*i<=n; i++)
-//         {
-//         while (n%i==0)
-//             {
-//             res.push_back(i);
-//             n/=i;
-//         }
-//     }
-//     if (n!=1) res.push_back(n);
-//     return res;
-// }
-// vector<int> factorize(int n)
-// {
-//     vector <int> res;
-//     for (int i=2; i*i<=n; i++)
-//         {
-//         while (n%i==0)
-//             {
-//             res.push_back(i);
-//             n/=i;
-//         }
-//     }
-//     if (n!=1) res.push_back(n);
-//     return res;
-// }
+
+vector<int> factorize(int n)
+{
+	vector<int> res;
+	for (int i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			res.push_back(i);
+			n /= i;
+		}
+	}
+	if (n != 1)
+		res.push_back(n);
+	return res;
+}
 //* Delete
 /*var a = document.querySelectorAll(".MJX_Assistive_MathML")
 a.forEach(s=> s.remove())
@@ -126,6 +120,32 @@ bool isPrime(int n)
 
 	return true;
 }
+ll func(ll n)
+{
+	ll sum = 0;
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+bool cmp(ll l, ll r)
+{
+	return l > r;
+}
+ll binpow(ll a, ll b)
+{
+	ll ans = 1ll;
+	while (b > 0)
+	{
+		if (b & 1)
+			ans *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return ans;
+}
 
 void solve()
 {
@@ -150,18 +170,14 @@ void solve()
 	}
 	cout << mins << endl;
 }
-
 int main()
 {
-
 	fast_out();
 	int t = 1;
 	cin >> t;
 	wh(t--)
 	{
-
 		solve();
-
 		// cout << endl;
 	}
 
